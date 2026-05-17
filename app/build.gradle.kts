f0434683a30b59dc129d8e6784664c3eb4cccb1a
afei <165532911+afeimod@users.noreply.github.com>
2026-05-17 14:24:42 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
